743e129da6f3b72bc49db2eb5cf53fcbdd2b5f27
Tyson Walker II <tywalk10@gmail.com>
2024-01-30 16:23:23 -0500

    here is the game

---
+++ js/utils.js
@@ -0,0 +1,25 @@
+Array.prototype.parse2D = function(data){
+    const rows = [];
+    for( let i = 0; i < this.length; i+= 16){
+        rows.push(this.slice(i, i + 16))
+    }
+    return rows
+}
+
+Array.prototype.createObjectsFrom2D = function () {
+    const objects = []
+    this.forEach((row, y) => {
+        row.forEach((symbol, x) => {
+            if (symbol === 292){
+                //push a new collision into the thing
+                objects.push(new CollisionBlock({
+                    position: {
+                        x:x * 64,
+                        y:y * 64,
+                    },
+                }))
+            }
+        })
+        })
+        return objects
+}
